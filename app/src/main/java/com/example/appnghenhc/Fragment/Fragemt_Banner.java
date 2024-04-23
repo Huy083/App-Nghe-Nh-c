6a23c0d6f55ea696b6b96a603b60f4ab329388d7
Huy <phamquanghuy2002.quatlam@gmail.com>
2024-04-24 02:59:40 +0700

    commit lan 3

--- app/src/main/java/com/example/appnghenhc/Fragment/Fragemt_Banner.java
+++ app/src/main/java/com/example/appnghenhc/Fragment/Fragemt_Banner.java
@@ -1,14 +1,17 @@
 package com.example.appnghenhc.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.appnghenhc.Adapter.BannerAdapter;
 import com.example.appnghenhc.Model.Quangcao;
 import com.example.appnghenhc.R;
 import com.example.appnghenhc.Service.APIService;
@@ -17,6 +20,7 @@ import com.example.appnghenhc.Service.Dataservice;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.relex.circleindicator.CircleIndicator;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,13 +28,26 @@ import retrofit2.Response;
 public class Fragemt_Banner extends Fragment {
 
     View view;
+    ViewPager viewPager;
+    CircleIndicator circleIndicator;
+    BannerAdapter bannerAdapter;
+    Runnable runnable;
+    Handler handler;
+    int currentItem;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragemt_banner,container,false);
+        anhxa();
         GetData();
         return view;
     }
+
+    private void anhxa() {
+        viewPager = view.findViewById(R.id.viewpager);
+        circleIndicator = view.findViewById(R.id.indicatordefault);
+    }
+
     private void GetData() {
         Dataservice dataservice = APIService.getService();
         Call<List<Quangcao>> callback = dataservice.GetDataBanner();
@@ -38,7 +55,23 @@ public class Fragemt_Banner extends Fragment {
             @Override
             public void onResponse(Call<List<Quangcao>> call, Response<List<Quangcao>> response) {
                 ArrayList<Quangcao> banners = (ArrayList<Quangcao>) response.body();
-                Log.d("BBB",banners.get(0).getTenBaiHat());
+                bannerAdapter = new BannerAdapter(getActivity(),banners);
+                viewPager.setAdapter(bannerAdapter);
+                circleIndicator.setViewPager(viewPager);
+                handler = new Handler();
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        currentItem = viewPager.getCurrentItem();
+                        currentItem++;
+                        if(currentItem >= viewPager.getAdapter().getCount()){
+                            currentItem = 0;
+                        }
+                        viewPager.setCurrentItem(currentItem,true);
+                        handler.postDelayed(runnable,4500);
+                    }
+                };
+                handler.postDelayed(runnable,4500);
             }
 
             @Override
