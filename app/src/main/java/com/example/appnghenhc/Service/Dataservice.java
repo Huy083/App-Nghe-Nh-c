8f25f2e279fbb7242480445f07a179ce4404f66b
Huy <phamquanghuy2002.quatlam@gmail.com>
2024-05-05 18:17:15 +0700

    tiếp tục, đọc dữ liệu API danh sách bài hát từ quảng cáo và gắn dữ liệu lên giao diện => Khi click vào bài hát trên phần quảng cáo thì nó sẽ hiện ra giao diện của bàn hát đó

--- app/src/main/java/com/example/appnghenhc/Service/Dataservice.java
+++ app/src/main/java/com/example/appnghenhc/Service/Dataservice.java
@@ -9,7 +9,10 @@ import com.example.appnghenhc.Model.Theloaitrongngay;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface Dataservice {
 
@@ -27,5 +30,9 @@ public interface Dataservice {
 
     @GET("baihatduocthich.php")
     Call<List<Baihat>> GetBaiHatHot();
+
+    @POST("danhsachbaihat.php")
+    @FormUrlEncoded
+    Call<List<Baihat>> GetDanhsachbaihattheoquangcao(@Field("idquangcao") String idquangcao);
 
 }
--- app/src/main/java/com/example/appnghenhc/activity/DanhsachbaihatActivity.java
+++ app/src/main/java/com/example/appnghenhc/activity/DanhsachbaihatActivity.java
@@ -1,6 +1,10 @@
 package com.example.appnghenhc.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -8,12 +12,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.appnghenhc.Model.Baihat;
 import com.example.appnghenhc.Model.Quangcao;
 import com.example.appnghenhc.R;
+import com.example.appnghenhc.Service.APIService;
+import com.example.appnghenhc.Service.Dataservice;
+import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DanhsachbaihatActivity extends AppCompatActivity {
 
@@ -23,14 +43,67 @@ public class DanhsachbaihatActivity extends AppCompatActivity {
     RecyclerView recyclerViewdanhsachbaihat;
     FloatingActionButton floatingActionButton;
     Quangcao quangcao;
-
+    ImageView imgdanhsachcakhuc;
+    ArrayList<Baihat> mangbaihat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danhsachbaihat);
         DataIntent();
         anhxa();
+        init();
+        if(quangcao != null && !quangcao.getTenBaiHat().equals("")){
+            setValueInView(quangcao.getTenBaiHat(),quangcao.getHinhBaiHat());
+            GetDataQuangCao(quangcao.getIdQuangCao());
+        }
     }
+
+    private void GetDataQuangCao(String idquangcao) {
+        Dataservice dataservice = APIService.getService();
+        Call<List<Baihat>> callback = dataservice.GetDanhsachbaihattheoquangcao(idquangcao);
+        callback.enqueue(new Callback<List<Baihat>>() {
+            @Override
+            public void onResponse(Call<List<Baihat>> call, Response<List<Baihat>> response) {
+                mangbaihat = (ArrayList<Baihat>) response.body();
+                Log.d("BBB",mangbaihat.get(0).getTenbaihat());
+            }
+
+            @Override
+            public void onFailure(Call<List<Baihat>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void setValueInView(String ten, String hinh) {
+        collapsingToolbarLayout.setTitle(ten);
+        try {
+            URL url = new URL(hinh);
+            Bitmap bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(),bitmap);
+            collapsingToolbarLayout.setBackground(bitmapDrawable);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Picasso.with(this).load(hinh).into(imgdanhsachcakhuc);
+    }
+
+    private void init() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        collapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
+
+    }
+
 
     private void anhxa() {
         coordinatorLayout = findViewById(R.id.coordinatorlayout);
@@ -38,6 +111,7 @@ public class DanhsachbaihatActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbardanhsach);
         recyclerViewdanhsachbaihat = findViewById(R.id.recyclerviewdanhsachbaihat);
         floatingActionButton = findViewById(R.id.floatingactionbotton);
+        imgdanhsachcakhuc = findViewById(R.id.imageviewdanhsachcakhuc);
     }
 
     private void DataIntent() {
@@ -50,3 +124,4 @@ public class DanhsachbaihatActivity extends AppCompatActivity {
         }
     }
 }
+
