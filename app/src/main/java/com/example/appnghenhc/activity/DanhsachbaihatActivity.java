aa98c9c882a6e446db9752612d733ca979bbc960
Huy <phamquanghuy2002.quatlam@gmail.com>
2024-05-05 15:38:37 +0700

    tiếp tục thiết kế giao diện cho màn hình danh sách bài hát,

--- app/src/main/java/com/example/appnghenhc/activity/DanhsachbaihatActivity.java
+++ app/src/main/java/com/example/appnghenhc/activity/DanhsachbaihatActivity.java
@@ -1,8 +1,13 @@
 package com.example.appnghenhc.activity;
 
 import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.example.appnghenhc.Model.Quangcao;
@@ -12,6 +17,11 @@ import java.io.Serializable;
 
 public class DanhsachbaihatActivity extends AppCompatActivity {
 
+    CoordinatorLayout coordinatorLayout;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    Toolbar toolbar;
+    RecyclerView recyclerViewdanhsachbaihat;
+    FloatingActionButton floatingActionButton;
     Quangcao quangcao;
 
     @Override
@@ -19,6 +29,15 @@ public class DanhsachbaihatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danhsachbaihat);
         DataIntent();
+        anhxa();
+    }
+
+    private void anhxa() {
+        coordinatorLayout = findViewById(R.id.coordinatorlayout);
+        collapsingToolbarLayout = findViewById(R.id.collapsingtoolbar);
+        toolbar = findViewById(R.id.toolbardanhsach);
+        recyclerViewdanhsachbaihat = findViewById(R.id.recyclerviewdanhsachbaihat);
+        floatingActionButton = findViewById(R.id.floatingactionbotton);
     }
 
     private void DataIntent() {
